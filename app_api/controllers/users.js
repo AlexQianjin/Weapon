2573d4abc9a4b8ca8448cc63998f60304527105c
Alex Qin <qianjin.qin@qq.com>
2016-06-19 07:03:39 -0700

    set nodemon

--- app_api/controllers/users.js
+++ app_api/controllers/users.js
@@ -7,3 +7,8 @@ module.exports.getUsers = function (req, res) {
     var users = {"users":[{"id":1,"name":"Alex"},{"id":2,"name":"Qianjin"}]};
     sendJSONresponse(res, 200, users);
 }
+
+module.exports.getUser = function (req, res) {
+    var user = {"id":3, "name":"AlexQin"};
+    sendJSONresponse(res, 200, user);
+}
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -3,5 +3,6 @@ var router = express.Router();
 var ctrlUsers = require('../controllers/users')
 
 router.get('/users', ctrlUsers.getUsers);
+router.get('/user', ctrlUsers.getUser);
 
 module.exports = router;
