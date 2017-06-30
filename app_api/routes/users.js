f294d09ce09aa1e8b1f5f53571554794f2376c77
Alex Qin <qianjin.qin@qq.com>
2017-06-30 07:58:05 -0700

    code: update user controller

--- app_api/routes/users.js
+++ app_api/routes/users.js
@@ -1,22 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
+var ctrlUsers = require('../controllers/users')
 
 router.get('/',
         passport.authenticate('jwt-bearer', { session: false }),
-        function (req, res) {
-            userDb.find({
-            }, { password: 0 }, function (err, data) {
-                    if(err){
-                        res.json(err);
-                    }
-                    if(data.length > 0){
-                        res.json(data);
-                    }
-                    else{
-                        res.json({"message": "User doesn't exist!"})
-                    }
-                });
-        });
+        ctrlUsers.getUsers);
 
 module.exports = router;
