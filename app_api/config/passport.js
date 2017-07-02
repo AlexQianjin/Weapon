eb04d10e07c6766770437e4ac331d894574300c9
Alex Qin <qianjin.qin@qq.com>
2017-07-02 07:25:40 -0700

    code: add passport config

---
+++ app_api/config/passport.js
@@ -0,0 +1,20 @@
+var passport = require('passport');
+var JwtBearerStrategy = require('passport-http-jwt-bearer');
+var mongoose = require('mongoose');
+var config = require('../../config');
+var userDb = mongoose.model('users', userSchema);
+
+passport.use(new JwtBearerStrategy(
+   config.secretOrPublicKey,
+   function(token, done) {
+     userDb.findById(token.sub, function (err, user) {
+       if (err) { return done(err); }
+       if (!user) { return done(null, false); }
+    //    if(token.exp < Math.floor(Date.now() / 1000))
+    //    { 
+    //        return done(null, false, 'Token has expired!');
+    //    }
+       return done(null, user, token);
+     });
+   }
+ ));
