0af9129a812a96fe7c0cdf4ad32ca509b5264a6a
Alex Qin <qianjin.qin@qq.com>
2018-05-03 11:23:27 +0800

    add file

---
+++ install.sh
@@ -0,0 +1,12 @@
+echo 'add keyserver'
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+
+echo 'update'
+
+sudo apt-get update
+
+echo 'install mongodb-org'
+sudo apt-get install -y mongodb-org
