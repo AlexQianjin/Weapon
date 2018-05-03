22e2a28cb169402aee010fa0cf21b0120f06d807
Alex Qin <qianjin.qin@qq.com>
2018-05-03 15:07:56 +0800

    add install nodejs scripts

--- install.sh
+++ install.sh
@@ -1,20 +1,40 @@
 #!/bin/bash  
-echo "This is a shell script"  
-ls -lah  
-echo "I am done running ls"  
-SOMEVAR='text stuff'  
-echo "$SOMEVAR"
+# echo "This is a shell script"  
+# ls -lah  
+# echo "I am done running ls"  
+# SOMEVAR='text stuff'  
+# echo "$SOMEVAR"
 
-echo "add keyserver"
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+###########################
+#
+#Install Nodejs
+#
+###########################
 
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+echo "Install Nodejs"
 
-echo "update"
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 
-sudo apt-get update
+sudo apt-get install -y nodejs
 
-echo "install mongodb-org"
+###########################
+#
+#Install MongoDB
+#
+###########################
+# echo "Install MongoDB"
 
-sudo apt-get install -y mongodb-org
+# echo "add keyserver"
+
+# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+
+# echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+
+# echo "update"
+
+# sudo apt-get update
+
+# echo "install mongodb-org"
+
+# sudo apt-get install -y mongodb-org
