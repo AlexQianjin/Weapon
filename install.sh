ab308aa90977a47fafb8bea897046a8ae306337d
Alex Qin <qianjin.qin@qq.com>
2018-05-03 17:02:52 +0800

    update script for docker

--- install.sh
+++ install.sh
@@ -10,6 +10,9 @@
 #Install Docker CE
 #
 ###########################
+sudo apt-get update
+
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
