7050409f09bbf62fa0f091da8a08c2807b79ab16
Alex Qin <qianjin.qin@qq.com>
2018-11-12 14:07:27 +0800

    update client to remove create-react-app

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-class App extends Component {
-	render() {
-		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Welcome to React</h1>
-				</header>
-				<p className="App-intro">
-					To get started, edit <code>src/App.js</code> and save to reload. Update to tab.
-				</p>
-			</div>
-		);
-	}
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { name: '', data: '' };
+
+        this.renderData = this.renderData.bind(this);
+    }
+
+    componentDidMount() {
+        console.log('App did mount');
+        axios.get('http://localhost:3000/api/v1/user')
+            .then(response => {
+                let data = response.data[0];
+                this.setState({name: data.username, data: data});
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+    
+    renderData() {
+        return this.state.name || 'Loading...' ;
+    }
+
+    render() {
+        return (<div>
+            Hello, React!
+            <p>{this.renderData()}</p>
+        </div>);
+    }
 }
 
 export default App;
