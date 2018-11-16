acbdc1c0e6dcdc47b23d1f00830fe689758e6b59
Alex Qin <qianjin.qin@qq.com>
2018-11-17 00:39:40 +0800

    update mongo script

--- scripts/mongo/initmongo.js
+++ scripts/mongo/initmongo.js
@@ -2,7 +2,9 @@ db.users.insert(
     {
         "username" : "alex",
         "password" : "cf03b3d0"
-    },
+    }
+);
+db.users.insert(
     {
         "username" : "doris",
         "password" : "cf03b3d0"
